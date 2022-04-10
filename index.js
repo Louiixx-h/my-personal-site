38090ef4d06e9314b96372fc1a1ef31013f93509
Louiixx-h <louix.sm@gmail.com>
2022-04-10 20:21:29 -0300

    update

--- index.js
+++
@@ -1,8 +0,0 @@
-const customExpress = require('./config/customExpress')
-
-const app = customExpress()
-const port = '5005'
-
-app.listen(port, () => {
-    console.log(`servidor rodando na url http://localhost:${port}`)
-})
